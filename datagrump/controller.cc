87fc626a9108790ac48d88a2628c2578a47eaa43
Greg Hill <gdh39@cornell.edu>
2015-02-23 01:08:39 -0800

    38 omg

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -165,5 +165,5 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
 {
-  return 200; /* timeout of one second */
+  return 180; /* timeout of one second */
 }
